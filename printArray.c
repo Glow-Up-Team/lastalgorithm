0a7788846c6c52a362daa1e978e88bcaa90a7b3d
JavierCan <2109022@upy.edu.mx>
2022-04-08 11:09:08 -0500

    printarray finished

---
+++ printArray.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "printarray.h"
+
+
+int main(){
+    int arr1[] = {1,2,3,4,5,6};
+    int arr2[] = {6,5,4,1,2,3};
+
+    printArray(arr1, 6);
+    //int min1 = min(arr1, 6);
+    //int min2 = min(arr2, 6);
+}
